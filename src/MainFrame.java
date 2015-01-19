987b63e98d791c03e9a7ebe7192a90ba51e5c272
timur560 <timur560@gmail.com>
2015-01-19 21:19:20 +0200

    some changes

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -9,6 +9,7 @@ public class MainFrame extends JFrame {
 
     public MainFrame() {
         super();
+        setTitle("Ping-pong game");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(500, 700);
         gamePane.setBackground(Color.WHITE);
